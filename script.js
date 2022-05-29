6a0f5b0f55621a17353043a3295c3e5d29939ae7
Joshua Ibbotson <josh@Joshuas-MacBook-Pro-2.local>
2022-05-29 17:11:13 +0100

    calculator '+' button officially works with '=' button but only for the number 1, all other buttons are not set up yet, most likely a function is required that utilises parameters that adjust by each int button, as currently it's setup using the 1 button as an event function, which would require each button setting up this way, which is extremly unnecessary

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
-let currentInt = 10;
+let currentInt = [];
+let currentIntArr;
 let nextInt = 10;
 let PositiveOrNegative = false;
 let plusToken = false;
+let minusToken = false;
 
 const display = document.getElementById('displayScreen')
 const AC = document.getElementById('AC').addEventListener('click', (e) => {
@@ -20,15 +22,44 @@ const posOrNeg = document.getElementById('+/-').addEventListener('click', (e) =>
 })
 const plus = document.getElementById('+').addEventListener('click', (e) => {
     plusToken = !plusToken
+    nextInt = currentInt
+    currentInt = 0
+    updateDisplay()
+})
+const minus = document.getElementById('-').addEventListener('click', (e) => {
+    minusToken = !minusToken
 })
 const equals = document.getElementById('=').addEventListener('click', (e)=> {
     if (plusToken === true) {
         plusToken = false;
-        currentInt = currentInt + nextInt;
+        currentInt += nextInt;
+    }
+    if (minusToken === true) {
+        minusToken = false;
+        currentInt -= nextInt
     }
     
     updateDisplay();
 })
+const one = document.getElementById('number1').addEventListener('click', (e) => {
+    if (typeof currentInt !== 'object'){
+        currentIntArr = String(currentInt).split("").map((currentInt) => {
+            return Number(currentInt)
+        })
+				currentIntArr.push(1)
+        currentInt = +currentIntArr.join("");
+        updateDisplay()
+    }
+    else if (typeof currentInt === 'object'){
+        currentInt.push(1)
+        currentInt = +currentInt.join("");
+        console.log(currentInt)
+        updateDisplay()
+    }
+})
+
+
+
 function updateDisplay() {
     display.innerHTML = currentInt;
 }
