ce45c60d41c06d8937d6cd5e60e3bceb7e660b8a
Joshua Ibbotson <josh@Joshuas-MacBook-Pro-2.local>
2022-05-29 16:27:12 +0100

    plus sign given functionality, alongside equals sign and negative or positive button.

--- script.js
+++ script.js
@@ -1,3 +1,34 @@
-const display = document.getElementById('displayScreen').addEventListener('click', (e) =>{
-    console.log(this.id)
+let currentInt = 10;
+let nextInt = 10;
+let PositiveOrNegative = false;
+let plusToken = false;
+
+const display = document.getElementById('displayScreen')
+const AC = document.getElementById('AC').addEventListener('click', (e) => {
+    currentInt = 0;
+    updateDisplay();
 })
+const posOrNeg = document.getElementById('+/-').addEventListener('click', (e) => {
+    PositiveOrNegative = !PositiveOrNegative
+    if (PositiveOrNegative === true){
+        currentInt = Math.abs(currentInt)
+    }
+    else if (PositiveOrNegative === false){
+        currentInt = -Math.abs(currentInt)
+    };
+    updateDisplay()
+})
+const plus = document.getElementById('+').addEventListener('click', (e) => {
+    plusToken = !plusToken
+})
+const equals = document.getElementById('=').addEventListener('click', (e)=> {
+    if (plusToken === true) {
+        plusToken = false;
+        currentInt = currentInt + nextInt;
+    }
+    
+    updateDisplay();
+})
+function updateDisplay() {
+    display.innerHTML = currentInt;
+}
